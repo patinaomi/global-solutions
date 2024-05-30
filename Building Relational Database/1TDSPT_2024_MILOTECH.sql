c726757e5dc6404e370ba1153108d9abd4107b7c
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-05-30 16:06:18 -0300

    Update and rename Final_Global_Solution_2024.sql to 1TDSPT_2024_MILOTECH.sql

---
+++ Building Relational Database/1TDSPT_2024_MILOTECH.sql
@@ -0,0 +1,8 @@
+--AQUI SERA O CODIGO OFICIAL PARA A ENTREGA
+
+-- Nome dos Integrantes: 
+-- Claudio Silva Bispo RM553472
+-- Patricia Naomi Yamagishi RM552981
+-- Sabrina da Motta Café RM553568
+
+-- Drop das tabelas caso elas existam
