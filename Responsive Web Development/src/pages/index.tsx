978b2699512f61f3a3f559c0835fff7cc7bb29cd
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-06-07 18:47:25 -0300

    arquivo index

---
+++ Responsive Web Development/src/pages/index.tsx
@@ -0,0 +1,30 @@
+
+
+import SessaoPrincipal from "@/components/SessaoPrincipal"
+import Missao from "@/components/Missao"
+import SobreProjeto from "@/components/SobreProjeto"
+import Funcionalidades from "@/components/Funcionalidades"
+import Dados from "@/components/Dados"
+import SobreMissao from "@/components/SobreMissao"
+import Eventos from "@/components/Eventos"
+import Projeto from "@/components/Projeto"
+import Educacao from "@/components/Educacao"
+
+export default function Home() {
+
+  return (
+    <div> 
+      
+      <SessaoPrincipal/>
+      <Missao/>
+      <SobreProjeto/>
+      <Funcionalidades/>
+      <Dados/>
+      <SobreMissao/>
+      <Eventos/>
+      <Projeto/>
+      <Educacao/>
+      
+    </div>
+  )
+}
